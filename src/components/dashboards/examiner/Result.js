11ee94b41b6b3c4593c69f69307a17623537e208
lightpc <afoageektech@gmail.com>
2024-05-18 05:40:26 -0700

    added getCourse() as dependencies

--- src/components/dashboards/examiner/Result.js
+++ src/components/dashboards/examiner/Result.js
@@ -19,7 +19,7 @@ const Result = () => {
         if (coursesData) {
             getCourse()
         }
-    }, [activeCode, coursesData])
+    }, [activeCode, coursesData, getCourse()])
 
     const handleActiveCode = (code) => {
         setActiveCode(() => { return code })

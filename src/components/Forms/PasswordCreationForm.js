2b7a19b83a8fab03eb89c8f2f5305e1cbdb3decf
lightpc <afoageektech@gmail.com>
2024-08-11 01:09:35 +0100

    Added Navbar component to PasswordCreationForm, updated form layout and styling.

--- src/components/Forms/PasswordCreationForm.js
+++ src/components/Forms/PasswordCreationForm.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import Navbar from "../navbar/Navbar";
 
 const PasswordCreationForm = () => {
     const router = useRouter()
@@ -38,87 +39,95 @@ const PasswordCreationForm = () => {
 
 
     return (
-      <div className="bg-slate-300 h-[400px] w-3/12 flex flex-col text-slate-800 justify-center items-center gap-3 rounded-md shadow-md py-5 z-30">
-        <h1 className="font-bold text-lg"> Staff Password Creation Form</h1>
-        {/* Password creation form*/}
-        <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-8/12">
-          {/* email input */}
-          <span>
-            <label className="text-sm" htmlFor="email">
-              Email:
-            </label>
-            <input
-              value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-              type="text"
-              name="email"
-              placeholder="johndoe@domain.com"
-              className="p-2 rounded-md "
-              required
-            />
-          </span>
-
-          <span>
-            <label className="text-sm" htmlFor="password">
-              OTP
-            </label>
-            {/* temporary password input */}
-            <input
-              value={formData.tempPwd}
-              onChange={(e) =>
-                setFormData({ ...formData, tempPwd: e.target.value })
-              }
-              type="text"
-              name="tempPwd"
-              placeholder="ot-123456"
-              className="p-2 rounded-md "
-            />
-          </span>
-
-          <hr className="border-1 border-slate-400 mx-5" />
-
-          <span>
-            <label className="text-sm" htmlFor="password">
-              password
-            </label>
-            {/* password input */}
-            <input
-              value={formData.pwd}
-              onChange={(e) => {
-                setFormData({ ...formData, pwd: e.target.value });
-              }}
-              type="password"
-              name="password"
-              placeholder="password"
-              className="p-2 rounded-md "
-            />
-          </span>
-
-          <span>
-            <label className="text-sm" htmlFor="confirmPassword">confirm password</label>
-            {/* confirm password input */}
-            <input
-              value={formData.confirmPwd}
-              onChange={(e) =>
-                setFormData({ ...formData, confirmPwd: e.target.value })
-              }
-              type="password"
-              name="confirmPwd"
-              placeholder="password"
-              className="p-2 rounded-md "
-            />
-          </span>
-
-          <button
-            type="submit"
-            className="bg-slate-800 p-1 rounded-md shadow-md font-semibold text-lg text-white hover:text-gray-800 hover:bg-white"
+      <div className="w-full h-full z-30 flex flex-col items-center ">
+        <Navbar/>
+        <div className="bg-slate-300 h-[400px] w-3/12 flex flex-col text-slate-800 justify-center items-center gap-2 rounded-md shadow-md py-5 overflow-auto mt-auto ">
+          <h1 className="font-bold text-lg"> Staff Password Creation Form</h1>
+          {/* Password creation form*/}
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-1 w-8/12 items-center"
           >
-            {loading ? `Loading...` : `Create Password`}
-          </button>
-          {/* <Link href="/">First Login? Create Your Password</Link> */}
-        </form>
+            {/* email input */}
+            <span>
+              <label className="text-sm" htmlFor="email">
+                Email:
+              </label>
+              <input
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+                type="text"
+                name="email"
+                placeholder="johndoe@domain.com"
+                className="py-1 px-2 rounded-md "
+                required
+              />
+            </span>
+
+            <span>
+              <label className="text-sm" htmlFor="password">
+                OTP
+              </label>
+              {/* temporary password input */}
+              <input
+                value={formData.tempPwd}
+                onChange={(e) =>
+                  setFormData({ ...formData, tempPwd: e.target.value })
+                }
+                type="text"
+                name="tempPwd"
+                placeholder="ot-123456"
+                className="py-1 px-2 rounded-md "
+              />
+            </span>
+
+            <hr className="border-1 border-slate-400 mx-5" />
+
+            <span>
+              <label className="text-sm" htmlFor="password">
+                password
+              </label>
+              {/* password input */}
+              <input
+                value={formData.pwd}
+                onChange={(e) => {
+                  setFormData({ ...formData, pwd: e.target.value });
+                }}
+                type="password"
+                name="password"
+                placeholder="password"
+                className="py-1 px-2 rounded-md "
+              />
+            </span>
+
+            <span>
+              <label className="text-sm" htmlFor="confirmPassword">
+                confirm password
+              </label>
+              {/* confirm password input */}
+              <input
+                value={formData.confirmPwd}
+                onChange={(e) =>
+                  setFormData({ ...formData, confirmPwd: e.target.value })
+                }
+                type="password"
+                name="confirmPwd"
+                placeholder="password"
+                className="py-1 px-2 rounded-md "
+              />
+            </span>
+
+            <button
+              type="submit"
+              className="bg-slate-800 py-1 px-5 rounded-md shadow-md text-white hover:text-gray-800 hover:bg-white mt-4"
+            >
+              {loading ? `Loading...` : `Create Password`}
+            </button>
+            {/* <Link href="/">First Login? Create Your Password</Link> */}
+          </form>
+        </div>
       </div>
     );
 };
